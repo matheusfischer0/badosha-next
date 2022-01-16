85fc2dc1eb4c323a953a1bf481465ce0c654d83d
Matheus Fischer <matheusfischerr@icloud.com>
2022-01-16 10:31:25 -0300

    mudanca no cardapio2

--- src/pages/api/menu.ts
+++ src/pages/api/menu.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 const menu = [
   {
     name: 'Pilsen Schorstein',
